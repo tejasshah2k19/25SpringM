161e5fca57c10995ae198eec93c39343e9b3987c
Tejas Shah <tejasshah2k19@gmail.com>
2025-07-18 18:06:26 +0530

    hangman

--- src/main/java/com/controller/HangManController.java
+++ src/main/java/com/controller/HangManController.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bean.WordBean;
 import com.dao.WordBeanDao;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HangManController {
@@ -33,11 +37,30 @@ public class HangManController {
 	}
 
 	@GetMapping("startgame")
-	public String startGame(Model model) {
+	public String startGame(Model model, HttpSession session) {
 
 		int index = (int) (Math.random() * WordBeanDao.words.size());
 		WordBean wb = WordBeanDao.words.get(index);
-		model.addAttribute("word",wb);
+		session.setAttribute("wb", wb);
+		session.setAttribute("word", wb.getWord());
+		session.setAttribute("life", 3);
+		session.setAttribute("guessCorrectChar", new ArrayList<>());
 		return "Game";
 	}
+
+	@PostMapping("checkword")
+	public String checkWord(String guessChar,HttpSession session) {
+		String word = (String)session.getAttribute("word");
+		int life = (int)session.getAttribute("life");
+		
+		if(word.toLowerCase().contains(guessChar.toLowerCase())) {
+				//compare => 
+			
+		}else {
+			life--;
+			session.setAttribute("life", life);
+		}
+		return "Game";
+	}
+
 }
--- src/main/webapp/WEB-INF/views/Game.jsp
+++ src/main/webapp/WEB-INF/views/Game.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,9 +10,22 @@
 </head>
 <body>
 
-${word.hint }<br>
-${word.word }<br>
-${word.difficulty}
+	${wb.hint } : Life ${life}
+	<br>
+	<br>  
+	<br>
+	
+	<c:forEach begin="1" end="${wb.word.length()}" step="1">
+	
+		________&nbsp;
+	
+	</c:forEach>
+<br><br> 
+      
+	<form action="checkword" method="post">
+		Guess Character : <input type="text" name="guessChar"/><br><br>
+		<input type="submit" value="Check"/>
+	</form>
 
 </body>
 </html>
